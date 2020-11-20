5fc28a7654d2bfe89f3878a8ab0f73dfac198d64
gabriele <gabrielescodinu96@gmail.com>
2020-11-20 15:32:49 +0100

    commit 01

---
+++ main.js
@@ -0,0 +1,21 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+var userWord = prompt("inserisci una parola")
+// Creare una funzione per capire se la parola inserita è palindroma
+// Pari e Dispari
+function isEven (number) {
+  if (userWord % 2 === 0) {
+    return true;
+  }
+}
+
+if (isEven(userWord)){
+  console.log(pari);
+} else {
+  console.log(dispari);
+}
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
