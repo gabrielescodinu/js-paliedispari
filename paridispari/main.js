3a50175d296670b917258de7ec1e750980c4043c
gabriele <gabrielescodinu96@gmail.com>
2020-11-20 16:05:39 +0100

    commit 03

--- paridispari/main.js
+++ paridispari/main.js
@@ -1,19 +1,24 @@
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 var oddEven = prompt("scegli pari o dispari");
+var yourNumber = number(prompt("scegli un numero da 1 a 5"));
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-var unoCinque = [1, 2, 3, 4, 5]
+var computerNumber = [1, 2, 3, 4, 5]
 
-function numeroCasuale (unoCinque) {
-  return unoCinque[Math.floor(Math.random()*unoCinque.length)];
+function numeroCasuale (computerNumber) {
+  return computerNumber[Math.floor(Math.random()*computerNumber.length)];
 }
-console.log(numeroCasuale(unoCinque));
+
+console.log(numeroCasuale(computerNumber));
+
 // Sommiamo i due numeri
-var sum = oddEven + unoCinque;
+var sum = yourNumber + computerNumber;
+
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 function result () {
   if (sum % 2 == 0) {
     return true;
   }
 }
+
 // Dichiariamo chi ha vinto.
