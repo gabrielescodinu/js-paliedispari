cd31a6f268a4d2e9aaa321ce7e5cea664f62974b
gabriele <gabrielescodinu96@gmail.com>
2020-11-20 16:18:15 +0100

    commit 04
    esercizio polidromo completato, esercizio del pari e dispari work in progress.

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 var userWord = prompt("inserisci una parola")
+
 // Creare una funzione per capire se la parola inserita è palindroma
 function palindrome(userWord) {
   var reversed = userWord.split("").reverse().join("");
-  console.log(reversed);
   if (reversed == userWord) {
     return true;
   } else {
     return false;
   }
 }
-console.log();
+var verifica = palindrome(userWord);
+console.log(verifica);
